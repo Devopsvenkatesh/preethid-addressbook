e6c460d68e23af7c16f8008deaaf182ad47f86b9
VENKATESH <skillshare1109@gmail.com>
2023-10-16 15:45:48 +0530

    try14

--- server-config.sh
+++ server-config.sh
@@ -1,13 +1,13 @@
  sudo yum install java-1.8.0-openjdk-devel -y
  sudo yum install git -y
  sudo yum install maven -y
- if [ -d "addressbook" ]
+ if [ -d "preethid-addressbook" ]
  then
   echo 'repo is cloned'
-  cd /home/ec2-user/addressbook
+  cd /home/ec2-user/preethid-addressbook
   git pull origin jenkins1
  else
   git clone https://github.com/Devopsvenkatesh/preethid-addressbook.git
  fi
- cd /home/ec2-user/addressbook
+ cd /home/ec2-user/preethid-addressbook
  mvn package
